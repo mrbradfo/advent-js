e1154d3e3cb714f3c190317c2a848d9d0ba10eb7
Matt Bradford <mrbradfo33@gmail.com>
2022-12-06 09:19:21 -0700

    init

--- day1/src/App.ts
+++ day1/src/App.ts
@@ -2,4 +2,22 @@ async function startTimer() {
   console.log("Starting timer...");
 }
 
-startTimer();
+
+const seconds = document.querySelector("seconds");
+const minutes = document.querySelector("minutes");
+
+const settings = document.querySelector("settings");
+const start = document.querySelector("start");
+
+
+
+// boolean to check if timer is running
+let isRunning: boolean = false;
+
+// add event listener to start button
+start.addEventListener("click", () => {
+  if (!isRunning) {
+    isRunning = true;
+    startTimer();
+  }
+}
