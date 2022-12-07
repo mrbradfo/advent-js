3b859e4041a615fefb158e601818e9f5067622d6
mrbradfo <mrbradfo33@gmail.com>
2022-12-07 16:38:24 -0700

    day 1

--- day1/dist/App.js
+++ day1/dist/App.js
@@ -1,17 +1,63 @@
 "use strict";
-async function startTimer() {
-    console.log("Starting timer...");
-}
-const seconds = document.querySelector("seconds");
-const minutes = document.querySelector("minutes");
-const settings = document.querySelector("settings");
-const start = document.querySelector("start");
+const seconds = document.querySelector(".seconds > input[type=number]");
+const minutes = document.querySelector(".minutes > input[type=number]");
+const settings = document.querySelector(".settings");
+const start = document.querySelector(".start");
 // boolean to check if timer is running
 let isRunning = false;
+let intervalId;
 // add event listener to start button
 start.addEventListener("click", () => {
     if (!isRunning) {
-        isRunning = true;
         startTimer();
     }
+    else {
+        stopTimer();
+    }
 });
+//settings is selected
+settings.addEventListener("click", () => {
+    if (isRunning) {
+        stopTimer();
+    }
+    seconds.disabled = seconds.disabled ? false : true;
+    minutes.disabled = minutes.disabled ? false : true;
+});
+//start timer
+async function startTimer() {
+    console.log("Starting timer...");
+    isRunning = true;
+    //toggle start text to stop
+    start.innerHTML = "Stop";
+    //disable settings and minutes from being changed
+    seconds.disabled = true;
+    minutes.disabled = true;
+    //total time in seconds
+    let totalTime = parseInt(seconds.value) + parseInt(minutes.value) * 60;
+    intervalId = setInterval(() => {
+        let sec = parseInt(seconds.value);
+        let min = parseInt(minutes.value);
+        //calculate seconds and minutes from total and pad with 0 if needed
+        seconds.value =
+            (totalTime % 60).toString().length === 1
+                ? "0" + (totalTime % 60).toString()
+                : (totalTime % 60).toString();
+        minutes.value =
+            Math.floor(totalTime / 60).toString() === "0"
+                ? "00"
+                : Math.floor(totalTime / 60).toString();
+        if (totalTime === 0) {
+            stopTimer();
+            console.log("Time's up!");
+        }
+        totalTime--;
+        console.log("Current time is: " + minutes.value + ":" + seconds.value);
+    }, 1000);
+}
+//stop timer
+async function stopTimer() {
+    console.log("Stopping timer...");
+    clearInterval(intervalId);
+    start.innerHTML = "Start";
+    isRunning = false;
+}
--- day1/src/App.ts
+++ day1/src/App.ts
@@ -1,23 +1,78 @@
-async function startTimer() {
-  console.log("Starting timer...");
-}
-
-
-const seconds = document.querySelector("seconds");
-const minutes = document.querySelector("minutes");
-
-const settings = document.querySelector("settings");
-const start = document.querySelector("start");
-
-
+const seconds = document.querySelector(
+  ".seconds > input[type=number]"
+) as HTMLInputElement;
+const minutes = document.querySelector(
+  ".minutes > input[type=number]"
+) as HTMLInputElement;
+const settings = document.querySelector(".settings") as HTMLInputElement;
+const start = document.querySelector(".start") as HTMLInputElement;
 
 // boolean to check if timer is running
-let isRunning: boolean = false;
+let isRunning = false;
+
+let intervalId: number | undefined;
 
 // add event listener to start button
 start.addEventListener("click", () => {
   if (!isRunning) {
-    isRunning = true;
     startTimer();
+  } else {
+    stopTimer();
   }
+});
+
+//settings is selected
+settings.addEventListener("click", () => {
+  if (isRunning) {
+    stopTimer();
+  }
+  seconds.disabled = seconds.disabled ? false : true;
+  minutes.disabled = minutes.disabled ? false : true;
+});
+
+//start timer
+async function startTimer() {
+  console.log("Starting timer...");
+  isRunning = true;
+
+  //toggle start text to stop
+  start.innerHTML = "Stop";
+
+  //disable settings and minutes from being changed
+  seconds.disabled = true;
+  minutes.disabled = true;
+
+  //total time in seconds
+  let totalTime = parseInt(seconds.value) + parseInt(minutes.value) * 60;
+
+  intervalId = setInterval(() => {
+    let sec = parseInt(seconds.value);
+    let min = parseInt(minutes.value);
+
+    //calculate seconds and minutes from total and pad with 0 if needed
+    seconds.value =
+      (totalTime % 60).toString().length === 1
+        ? "0" + (totalTime % 60).toString()
+        : (totalTime % 60).toString();
+    minutes.value =
+      Math.floor(totalTime / 60).toString() === "0"
+        ? "00"
+        : Math.floor(totalTime / 60).toString();
+
+    if (totalTime === 0) {
+      stopTimer();
+      console.log("Time's up!");
+    }
+
+    totalTime--;
+    console.log("Current time is: " + minutes.value + ":" + seconds.value);
+  }, 1000);
+}
+
+//stop timer
+async function stopTimer() {
+  console.log("Stopping timer...");
+  clearInterval(intervalId);
+  start.innerHTML = "Start";
+  isRunning = false;
 }
