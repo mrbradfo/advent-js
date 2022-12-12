b48f497443cc39283f72c26d639680f6591c61de
mrbradfo <mrbradfo33@gmail.com>
2022-12-12 15:27:28 -0700

    day 2 complete

--- day2/app.ts
+++ day2/app.ts
@@ -1,3 +1,5 @@
+const TAX_RATE = 0.1; // 10% tax rate
+
 //a type for the menu item
 type MenuItem = {
   name: string;
@@ -7,6 +9,7 @@ type MenuItem = {
   count: number;
 };
 
+//menu items
 const menuItems = [
   {
     name: "French Fries with Ketchup",
@@ -52,9 +55,11 @@ const menuItems = [
   },
 ];
 
-const menu = document.querySelector("ul.menu") as HTMLDivElement;
+//cart items
+let cartItems: MenuItem[] = [];
 
-const renderMenu = (menuItems: MenuItem[]) => {
+const menu = document.querySelector("ul.menu") as HTMLDivElement;
+const renderMenu = () => {
   let menuString = menuItems
     .map((menuItem, index) => {
       const dollars = Math.floor(menuItem.price / 100);
@@ -71,14 +76,14 @@ const renderMenu = (menuItems: MenuItem[]) => {
         </div>
         <div class="content">
           <p class="menu-item">${menuItem.name}</p>
-          <p class="price">${dollars}.${cents}</p>
+          <p class="price">$${dollars}.${cents}</p>
           ${
             menuItem.count > 0
-              ? `<button class="in-cart" onClick=addToCartOnClick()>
+              ? `<button class="in-cart">
           <img src="images/check.svg" alt="Check" />
           In Cart
         </button>`
-              : `<button class="add" onClick=addToCartOnClick()>
+              : `<button class="add" onClick=addToCartOnClick(${index})>
        Add To Cart
       </button>`
           }
@@ -91,8 +96,114 @@ const renderMenu = (menuItems: MenuItem[]) => {
   menu.innerHTML = menuString;
 };
 
-function addToCartOnClick() {
-  console.log("add to cart button clicked");
-  renderMenu(menuItems);
+function addToCartOnClick(index: number) {
+  console.log("adding item " + index + " to cart");
+  menuItems[index].count++;
+
+  //add only if not already in cart
+  if (!cartItems.includes(menuItems[index])) {
+    cartItems.push(menuItems[index]);
+  }
+
+  renderCart(cartItems);
 }
-renderMenu(menuItems);
+
+const cart = document.querySelector("ul.cart-summary") as HTMLDivElement;
+
+function renderCart(cartItems: MenuItem[]) {
+  console.log("rendering cart");
+  const cartString = cartItems
+    .map((menuItem, index) => {
+      //if item is not in cart, don't render it
+      if (menuItem.count <= 0) return "";
+
+      const totalPrice = menuItem.price * menuItem.count;
+      const dollars = Math.floor(totalPrice / 100);
+      const cents = totalPrice % 100;
+
+      return `<li>
+    <div class="plate">
+      <img
+        src="images/${menuItem.image}"
+        alt="${menuItem.alt}"
+        class="plate"
+      />
+      <div class="quantity">${menuItem.count}</div>
+    </div>
+    <div class="content">
+      <p class="menu-item">${menuItem.name}</p>
+      <p class="price">${menuItem.price}</p>
+    </div>
+    <div class="quantity__wrapper">
+      <button class="decrease" onClick="decreaseQuantity(${index})">
+        <img src="images/chevron.svg" />
+      </button>
+      <div class="quantity">${menuItem.count}</div>
+      <button class="increase" onClick="increaseQuantity(${index})">
+        <img src="images/chevron.svg" />
+      </button>
+    </div>
+    <div class="subtotal">$${dollars}.${cents}</div>
+  </li>`;
+    })
+    .join("");
+  cart.innerHTML = cartString;
+
+  renderMenu();
+  renderTotals();
+}
+
+//render totals at bottom of cart
+const subtotal = document.querySelector(
+  ".amount.price.subtotal"
+) as HTMLDivElement;
+const tax = document.querySelector(".amount.price.tax") as HTMLDivElement;
+const total = document.querySelector(".amount.price.total") as HTMLDivElement;
+function renderTotals() {
+  const subtotalAmt = cartItems
+    .map((menuItem) => {
+      return menuItem.count * menuItem.price;
+    })
+    .reduce((total, currentItem) => total + currentItem);
+
+  let taxAmt = ((TAX_RATE * subtotalAmt) / 100).toFixed(2);
+
+  subtotal.innerHTML = "$" + convertToDollarsAndCents(subtotalAmt).toFixed(2);
+  tax.innerHTML = "$" + taxAmt;
+  total.innerHTML =
+    "$" +
+    convertToDollarsAndCents(subtotalAmt + Number(taxAmt) * 100).toFixed(2);
+}
+
+// decreaseQuantity
+function decreaseQuantity(index: number) {
+  console.debug("decreasing quantity of item " + index);
+  menuItems[index].count--;
+  renderCart(menuItems);
+}
+
+// increaseQuantity
+function increaseQuantity(index: number) {
+  console.debug("increasing quantity of item " + index);
+  menuItems[index].count++;
+  renderCart(menuItems);
+}
+
+renderMenu();
+
+//helper function to convert cents to dollars and cents
+//convert unformated number to dollars and cents
+//eg 223 to 2.23
+function convertToDollarsAndCents(total: number): number {
+  const dollars = Math.floor(total / 100);
+  const cents: number = Math.round(total % 100);
+
+  let dollarsAndCents = "";
+  if (cents < 10) dollarsAndCents = dollars + "." + "0" + cents;
+  else dollarsAndCents = dollars + "." + cents;
+
+  //dollarsAndCents to number
+  const dollarsAndCentsAsNumber = Number(dollarsAndCents);
+  dollarsAndCentsAsNumber.toFixed(2);
+  return Number(dollarsAndCentsAsNumber);
+}
