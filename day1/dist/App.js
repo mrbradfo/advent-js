f010377123681f7b16eaff7174349f0265caf2ce
mrbradfo <mrbradfo33@gmail.com>
2023-02-22 16:28:24 -0700

    removing day1

--- day1/dist/App.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-async function startTimer() {
-    console.log("Starting timer...");
-}
-startTimer();
--- day1/src/App.ts
+++
@@ -1,5 +0,0 @@
-async function startTimer() {
-  console.log("Starting timer...");
-}
-
-startTimer();
